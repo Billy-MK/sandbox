7adc7d9858d1d94fa87c6da210f8aab52775d7a9
Billy Keating <wmontoothkeating@gmail.com>
2022-02-04 09:58:10 -0800

    Component outline

---
+++ react-user-practice/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './components/Form/Form'
+import UserList from './components/UserList/UserList'
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
